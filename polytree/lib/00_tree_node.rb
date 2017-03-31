4471daa4f83b984f0198676def3e0056d459ac7e
App Academy Student <student@appacademy.io>
2017-03-31 13:48:49 -0700

    Polytree node added

---
+++ polytree/lib/00_tree_node.rb
@@ -0,0 +1,59 @@
+class PolyTreeNode
+  attr_accessor :value, :parent, :children
+
+  def initialize(value)
+    @value, @parent, @children = value, nil, []
+  end
+
+  def parent
+    @parent
+  end
+
+  def parent=(node)
+    @parent.children.delete(self) unless @parent.nil?
+    @parent = node
+    return nil if node.nil?
+    node.children << self unless node.children.include?(self)
+  end
+
+  def children
+    @children
+  end
+
+  def add_child(child)
+    @children << child unless @children.include?(child)
+    child.parent = self
+  end
+
+  def remove_child(child)
+    raise ArgumentError, 'No child found' unless @children.include?(child)
+    @children.delete(child).parent = nil
+  end
+
+  def value
+    @value
+  end
+
+  def dfs(target)
+    return self if self.value == target
+    return nil if @children.empty?
+
+    @children.each do |child|
+      target_node = child.dfs(target)
+      return target_node unless target_node.nil?
+    end
+    nil
+  end
+
+  def bfs(target)
+    nodes = [self]
+    until nodes.empty?
+      node = nodes.shift
+      return node if node.value == target
+      node.children.each do |child|
+        nodes << child
+      end
+    end
+    nil
+  end
+end
